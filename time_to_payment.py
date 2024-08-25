fb18c9a32d973b18c11c22c8af5bcdadad152518
Cristhian Melo <rxtsel@hotmail.com>
2024-08-25 13:32:59 -0500

    chore: refactor script

--- time_to_payment.py
+++ time_to_payment.py
@@ -1,17 +1,20 @@
-# Convert our hours, minutes and seconds to decimal hours
+# Only replace the values of hours, minutes and seconds and the rate_per_hour:
 hours = 92
-minutes = 35 / 60
-seconds = 12 / 3600
-
-# Sum all hours
-total_hours = hours + minutes + seconds
-
-print("Total Ours: ", total_hours.__round__(2))
+minutes = 35
+seconds = 12
 
 # Rate per hour
 rate_per_hour = 17
 
+# Convert minutes and seconds to decimal
+minutes_to_decimal = minutes / 60
+seconds_to_decimal = seconds / 3600
+
+# Sum all hours
+total_hours = hours + minutes_to_decimal + seconds_to_decimal
+print("Total Ours: ", total_hours.__round__(2))
+
 # Calculate total payment
 total_payment = total_hours * rate_per_hour
-
 print("Total: ", "${:,.2f}".format(total_payment))
+
